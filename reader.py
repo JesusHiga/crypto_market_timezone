bfeb8cf20e5e6597081235a1b80ef90e4c0ecc18
Jesus Higa <faradahiga@gmail.com>
2022-08-29 14:22:06 -0500

    version que lee dtype float y date del csv

--- reader.py
+++ reader.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import numpy as np
 
 
-df = pd.read_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_WS.csv', sep=';')
+df = pd.read_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_WS.csv', sep=',',dtype={'Open':np.float64,'Close':np.float64,'Change':np.float64}, parse_dates=['Date'],encoding='utf-8',)
+df = pd.DataFrame(df)
+
 print(df)
+print(df.info())
+print(df.describe())
