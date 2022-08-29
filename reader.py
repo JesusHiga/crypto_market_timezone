84390162573c2098f474b0893cd6d24a73c8493b
Jesus Higa <faradahiga@gmail.com>
2022-08-28 23:02:52 -0500

    version funcional 1

--- reader.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_WS.csv', sep=';')
-print(df)
--- timestamp.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def timestamp(dt):
-    epoch = datetime.utcfromtimestamp(0)
-    return (dt - epoch).total_seconds() * 1000
-if __name__ == '__main__':
-    dt = datetime(2022,1,1,20,30,00)
-    tempo = timestamp(dt)
-    print(tempo)
