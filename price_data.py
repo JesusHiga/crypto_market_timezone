8521514a73d829840bca635ea8f769b4f7e55b24
Jesus Higa <faradahiga@gmail.com>
2022-08-31 16:55:49 -0500

    version actualizada con csv de todas las zonas

--- price_data.py
+++ price_data.py
@@ -121,10 +121,10 @@ class robo_binance:
         
         #imprimimos y guardamos el resultado final
         print(data_frame) 
-        data_frame.to_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_WS.csv',sep=',')        
+        data_frame.to_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_NY.csv',sep=',')        
         
 bot = robo_binance('btcusdt', '30m')
-pprint(bot.market_time(3))
+pprint(bot.market_time(300))
 
 
     
--- reader.py
+++ reader.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_WS.csv', sep=',',dtype={'Open':np.float64,'Close':np.float64,'Change':np.float64},parse_dates=['Date'],encoding='utf-8',)
+df = pd.read_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_NY.csv', sep=',',dtype={'Open':np.float64,'Close':np.float64,'Change':np.float64},parse_dates=['Date'],encoding='utf-8',)
 df = pd.DataFrame(df)
 
 print(df)
