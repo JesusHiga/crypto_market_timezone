279dc5677782067b9508fd217aff3536c7d2b83a
Jesus Higa <faradahiga@gmail.com>
2022-09-02 16:02:51 -0500

    version que compila todas las horas y zonas

--- data_cleaner.py
+++ data_cleaner.py
@@ -7,15 +7,28 @@ import json
 # print()
 
 
-def ox(x):   
-    df = pd.DataFrame({'a':[]}) 
-    for i in range(11):
-        # df1 = pd.DataFrame({'a':[1,2,3]}) 
-        df2 = pd.DataFrame({'a':[4+i,5+i,6+i]})
-        df = pd.concat([df,df2],ignore_index=True)
-    print(df)
+# def ox(x):   
+#     df = pd.DataFrame({'a':[]}) 
+#     for i in range(11):
+#         # df1 = pd.DataFrame({'a':[1,2,3]}) 
+#         df2 = pd.DataFrame({'a':[4+i,5+i,6+i]})
+#         df = pd.concat([df,df2],ignore_index=True)
+#     print(df)
 
-# ab = pd.DataFrame()
-# print(ab)
-if __name__=="__main__":
-    ox(5)
+# # ab = pd.DataFrame()
+# # print(ab)
+# if __name__=="__main__":
+#     ox(5)
+
+
+list_dict = [{'market':'new_york','oh':1641047400000,'ch':1641069000000},{'market':'london','oh':1641024000000,'ch':1641052800000},{'market':'tokyo','oh':1640995200000,'ch':1641015000000}]
+# list_dict = [{'new_york':1641047400000,'london':1641024000000,'tokyo':1640995200000}]
+# list_dict2 = [{'new_york_close':1641069000000,'london_close':1641052800000,'tokyo_close':1641015000000}]
+
+df = pd.DataFrame(columns=['market'])
+for i in range(len(list_dict)):
+    
+    df_market = pd.DataFrame(list_dict[i],columns=['market'],index=[0])
+    df = pd.concat([df,df_market], ignore_index=True)
+print(df)
+    
--- price_data.py
+++ price_data.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from binance.spot import Spot
+from numpy import empty
 import config
 import pandas as pd
 import json
@@ -75,45 +76,57 @@ class robo_binance:
         
         """nuevas dataframes, listas para concatenar"""  
         date = pd.DataFrame(columns=['Date'])
+        market = pd.DataFrame(columns=['market'])
         open = pd.DataFrame(columns=['Open'])
         close = pd.DataFrame(columns=['Close'])
         day = pd.DataFrame(columns=['Week_day'])
+        finish = pd.DataFrame(columns=['Finish'])
         
         """for loop para agregar dias al dataframe"""
         for i in range(x):
             
-            #Variables de horarios de apertura y cierre por pais
-            new_york = 1641047400000
-            new_york_close = 1641069000000
-            london = 1641024000000
-            london_close = 1641052800000
-            tokyo = 1640995200000
-            tokyo_close = 1641015000000
-            
-            df_open = pd.DataFrame(self.binance_client().klines(symbol=self.pair, interval=self.temporality, limit=limit, startTime=new_york+86400000*i), columns=['Open time','Open','High','Low','Close','Volume','Close time','Quote asset volume','Number of trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore'],dtype=float) 
-            df_open['Date'] = pd.to_datetime(df_open['Open time'],unit='ms').dt.strftime(('%Y-%m-%d')) 
-            df_day = df_open[['Open time']]
-            df_date = df_open[['Date']] 
-            df_open = df_open[['Open']]
-            
-            #Dataframe diferente para definir la hora de cierre
-            df_close = pd.DataFrame(self.binance_client().klines(symbol=self.pair, interval=self.temporality, limit=limit, startTime=new_york_close+86400000*i), columns=['Open time','Open','High','Low','Close','Volume','Close time','Quote asset volume','Number of trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore'],dtype=float) 
-            df_close = df_close[['Close']]
-            
-            #Concatenamos los dataframes induvidualmente con los nuevos
-            date = pd.concat([date,df_date], ignore_index=True)
-            open = pd.concat([open,df_open], ignore_index=True)
-            close = pd.concat([close,df_close], ignore_index=True)
-            day = pd.concat([day,df_day], ignore_index=True)
-            
+            list_dict = [
+                {'market':'new_york','oh':1641047400000,'ch':1641069000000},
+                {'market':'london','oh':1641027600000,'ch':1641054600000},
+                {'market':'tokyo','oh':1641002400000,'ch':1641031200000}
+                ]
+    
+            for j in range(len(list_dict)):
+                
+                df_open = pd.DataFrame(self.binance_client().klines(symbol=self.pair, interval=self.temporality, limit=limit, startTime=list_dict[j]['oh']+86400000*i), columns=['Open time','Open','High','Low','Close','Volume','Close time','Quote asset volume','Number of trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore'],dtype=float) 
+                df_open['Date'] = pd.to_datetime(df_open['Open time'],unit='ms').dt.strftime(('%Y-%m-%d'))     
+                df_day = df_open[['Open time']]
+                df_date = df_open[['Date']] 
+                df_open = df_open[['Open']]
+                df_market = pd.DataFrame(list_dict[j],columns=['market'],index=[0])
+                
+                #Dataframe diferente para definir la hora de cierre
+                df_close = pd.DataFrame(self.binance_client().klines(symbol=self.pair, interval=self.temporality, limit=limit, startTime=list_dict[j]['ch']+86400000*i), columns=['Open time','Open','High','Low','Close','Volume','Close time','Quote asset volume','Number of trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore'],dtype=float) 
+                df_close1 = df_close[['Close']]
+                df_close['Finish'] = pd.to_datetime(df_close['Close time'],unit='ms').dt.strftime(('%H:%M:%S')) 
+                df_close2 = df_close[['Finish']]
+                
+                #Concatenamos los dataframes induvidualmente con los nuevos
+                date = pd.concat([date,df_date], ignore_index=True)
+                market = pd.concat([market,df_market], ignore_index=True)
+                open = pd.concat([open,df_open], ignore_index=True)
+                close = pd.concat([close,df_close1], ignore_index=True)
+                day = pd.concat([day,df_day], ignore_index=True)
+                finish = pd.concat([finish,df_close2], ignore_index=True)
+                
+                print(finish,'\n')           
+                
         #concatenamos todos los dataframes en uno solo    
-        data_frame = pd.concat([date,open,close,day],axis=1)
+        data_frame = pd.concat([date,market,open,close,day,finish],axis=1)
         
         #agregamos dos columnas, week_day y change
         data_frame['Week_day'] = pd.to_datetime(data_frame['Open time'],unit='ms',utc=False).dt.day_name()
         data_frame['Change'] = data_frame['Close']/data_frame['Open']-1
+        data_frame['start'] = pd.to_datetime(data_frame['Open time'],unit='ms').dt.strftime(('%H:%M:%S')) 
+        # data_frame['finish'] = pd.to_datetime(df_close['Close time'],unit='ms').dt.strftime(('%H:%M:%S')) 
+        
         #ordenamos las columnas
-        data_frame = data_frame[['Date','Week_day','Open','Close','Change']]
+        data_frame = data_frame[['Date','market','Week_day','start','Finish','Open','Close','Change']]
         #convertimos en float las columnas de datos numericos
         data_frame['Open'] = data_frame['Open'].astype(float)
         data_frame['Close'] = data_frame['Close'].astype(float)
@@ -121,10 +134,10 @@ class robo_binance:
         
         #imprimimos y guardamos el resultado final
         print(data_frame) 
-        data_frame.to_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_NY.csv',sep=',')        
+        data_frame.to_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame.csv',sep=',')        
         
 bot = robo_binance('btcusdt', '30m')
-pprint(bot.market_time(300))
+pprint(bot.market_time(1))
 
 
     
