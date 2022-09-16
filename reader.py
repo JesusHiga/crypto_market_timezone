2a29743840e6f3104d5c435613a2416b4b17619b
Jesus Higa <faradahiga@gmail.com>
2022-09-16 08:59:15 -0500

    version solo london

--- reader.py
+++ reader.py
@@ -3,9 +3,18 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_NY.csv', sep=',',dtype={'Open':np.float64,'Close':np.float64,'Change':np.float64},parse_dates=['Date'],encoding='utf-8',)
-df = pd.DataFrame(df)
+# df = pd.read_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_frame_NY.csv', sep=',',dtype={'Open':np.float64,'Close':np.float64,'Change':np.float64},parse_dates=['Date'],encoding='utf-8',)
+# df = pd.DataFrame(df)
 
-print(df)
-print(df.info())
-print(df.describe())
+# print(df)
+# print(df.info())
+# print(df.describe())
+
+
+list_dict = [
+                {'market':'tokyo',   'oh':1641002400000,'ch':1641022200000},
+                {'market':'london',  'oh':1648263600000,'ch':1651028400000},
+                {'market':'new_york','oh':1641047400000,'ch':1641069000000},
+                ]
+
+print(list_dict[0]['market'])
--- timestamp.py
+++ timestamp.py
@@ -1,15 +1,41 @@
 from datetime import datetime
 import tkinter
 
+from pandas import to_datetime
+
 def timestamp(dt):
     epoch = datetime.utcfromtimestamp(0)
     return (dt - epoch).total_seconds() * 1000
+
 if __name__ == '__main__':
-    dt = datetime(2022,9,1,8,00,00)
+    # y=[2019,2020,2021,2022,2023,2024,2025]
+    # d=[10,8,14,13,12,10,9]
+    # d2=[3,1,7,6,5,3,2]
+    # for j,i in zip(y,d2) :   
+    dt = datetime(2022,1,3,8,00,00)
     tempo = timestamp(dt)
     print(tempo)
     print(dt)
-    
-    # NY 14:30, 20:30
-    # LND 08:00, 14:00 
+    a = 1667095200000
+    print(to_datetime(a,unit='ms').date())
+    print(to_datetime(a,unit='ms').time())
+        
     # TKY 02:00, 07:30 
+    # LND 08:00, 14:00 
+    # NY 14:30, 20:30
+    
+    
+    #enero
+    # list_dict = [
+    #             {'market':'tokyo',   'oh':1641002400000,'ch':1641022200000},
+    #             {'market':'london',  'oh':1641024000000,'ch':1641045600000},
+    #             {'market':'new_york','oh':1641047400000,'ch':1641069000000},
+    #             ]
+    
+    
+    # septiembre
+    # list_dict = [
+    #             {'market':'tokyo',   'oh':1661997600000,'ch':1662017400000},
+    #             {'market':'london',  'oh':1662019200000,'ch':1662040800000},
+    #             {'market':'new_york','oh':1662042600000,'ch':1662064200000},
+    #             ]
