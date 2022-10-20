ebc967896a348cdf5578d6379e8fd64dec6e5b4d
Jesus Higa <faradahiga@gmail.com>
2022-10-20 10:18:09 -0500

    version con json

--- reader.py
+++ reader.py
@@ -11,7 +11,8 @@ def compil():
     dataframe = dataframe[['Date','Week_day','Market','Start','Finish','Open','Close','Change','Change_pct','Volume']]    
     print(dataframe)
         
-    return dataframe.to_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_total.csv',sep=',',encoding='utf-8',columns=['Date','Week_day','Market','Start','Finish','Open','Close','Change','Change_pct','Volume'],index=False)    
+    # return dataframe.to_csv('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_total.csv',sep=',',encoding='utf-8',columns=['Date','Week_day','Market','Start','Finish','Open','Close','Change','Change_pct','Volume'],index=False)    
+    return dataframe.to_json('/Users/macbook/platzi/web_scraping_03/binance_bot/scripts/files/data_total.json')
     
 if __name__=='__main__':
     print(compil())    
